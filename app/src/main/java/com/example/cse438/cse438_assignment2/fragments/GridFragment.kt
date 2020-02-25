136394a39378f960575918975a125cffb1c6c49a
Admin <Admin@Admins-MacBook-Pro.local>
2020-02-24 22:49:57 -0600

    Created grid view with recycler view

--- app/src/main/java/com/example/cse438/cse438_assignment2/fragments/GridFragment.kt
+++ app/src/main/java/com/example/cse438/cse438_assignment2/fragments/GridFragment.kt
@@ -1,25 +1,33 @@
 package com.example.cse438.cse438_assignment2.fragments
 
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.cse438.cse438_assignment2.R
+import com.example.cse438.cse438_assignment2.adapter.GridItemAdapter
+import kotlinx.android.synthetic.main.fragment_grid.view.*
 
 
 class GridFragment : Fragment() {
-
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_grid, container, false)
+
+        val rootView = inflater.inflate(R.layout.fragment_grid, container, false)
+
+        recyclerView= rootView.findViewById(R.id.recyclerView) as RecyclerView
+
+//        recyclerView.layoutManager = LinearLayoutManager(activity)
+        recyclerView.layoutManager = GridLayoutManager(activity, 2)
+        recyclerView.adapter = GridItemAdapter()
+        return rootView
     }
-
-
 }
