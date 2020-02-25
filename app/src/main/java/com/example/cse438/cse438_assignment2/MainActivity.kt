1c529e91cc96667a5d516954daff44becc202c3e
Admin <Admin@Admins-MacBook-Pro.local>
2020-02-24 21:02:47 -0600

    Addin g dependencies and basic views

--- app/src/main/java/com/example/cse438/cse438_assignment2/MainActivity.kt
+++ app/src/main/java/com/example/cse438/cse438_assignment2/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.cse438.cse438_assignment2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cse438.cse438_assignment2.fragments.GridFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        //Start the grid fragment
+        val fragment = GridFragment()
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, fragment)
+        transaction.commit()
     }
 }
